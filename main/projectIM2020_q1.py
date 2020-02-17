3302a170aa43092d9c9147c150b95bdb1ccdf05b
asafjo23 <asafjo23@gmail.com>
2020-02-17 20:50:10 +0200

    Start working on q1

--- main/projectIM2020_q1.py
+++ main/projectIM2020_q1.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python
+import numpy as np
+from matplotlib import pyplot as plt
+import glob
+import cv2
 
 """
     @title :        Main for Q1
     @description :  Finding scale and remove it
     @output:        Original, Scale with dots, No scale.
 """
+
+
+def read_images():
+    filenames = [img for img in glob.glob("../images/q1/*.jpg")]
+    images = []
+    for filename in filenames:
+        images.append(cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2RGB))
+    return images
+
+
+def show_image(img):
+    plt.imshow(img)
+    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+    plt.show()
+
+
+pictures = read_images()
+show_image(pictures[0])
