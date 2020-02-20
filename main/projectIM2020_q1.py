1db55536c9d3ea5d725227c5021726bafb1ab977
asafjo23 <asafjo23@gmail.com>
2020-02-20 20:03:39 +0200

    Update projectIM2020_q1.py

--- main/projectIM2020_q1.py
+++ main/projectIM2020_q1.py
@@ -17,19 +17,29 @@ def read_images():
         images.append(cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2RGB))
     return images
 
-def show_image(img):
-    plt.imshow(img)
+def show_image(org, img):
+    plt.subplot(121)
+    plt.imshow(org, cmap='gray')
+    plt.subplot(122)
+    plt.imshow(img, cmap='gray')
     plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
     plt.show()
 
-def remove_ruler(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (1, 1), 0)
-    edges = cv2.Canny(blur, 100, 200)
-    return edges
+def remove_ruler(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 125, 200, apertureSize=3)
+    # Detect points that form a line
 
+    lines = cv2.HoughLinesP(edges, 0.5, np.pi / 180, 125, minLineLength=100, maxLineGap=20)
+    # Draw lines on the image
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 3)
+    return edges
 
 def main():
     pictures = read_images()
-    show_image(remove_ruler(pictures[0]))
+    show_image(pictures[0], remove_ruler(pictures[0]))
+    show_image(pictures[1], remove_ruler(pictures[1]))
+    show_image(pictures[2], remove_ruler(pictures[2]))
 main()
