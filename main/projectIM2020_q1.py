8237daec64f9bedad99035b37df33c3bfed5810e
asafjo23 <asafjo23@gmail.com>
2020-02-28 17:20:55 +0200

    Update projectIM2020_q1.py

--- main/projectIM2020_q1.py
+++ main/projectIM2020_q1.py
@@ -2,9 +2,9 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import glob
+
+import random as rng
 import cv2
-import scipy.stats
-import PIL.Image
 
 """
     @title :        Main for Q1
@@ -24,7 +24,7 @@ def show_image(org, img):
     plt.imshow(org, cmap='gray')
     plt.subplot(122)
     plt.imshow(img, cmap='gray')
-    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+    # plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
     plt.show()
 
 def remove_ruler(image):
@@ -32,27 +32,58 @@ def remove_ruler(image):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     kernel = np.ones((5, 5), np.uint8)
     erosion = cv2.erode(gray, kernel, iterations=1)
-    edges = cv2.Canny(erosion, 75, 150, apertureSize=3)
+    edges = cv2.Canny(erosion, 50, 150, apertureSize=3)
     # Detect points that form a line
-    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 30, minLineLength=500, maxLineGap=50)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 30, minLineLength=1000, maxLineGap=10)
     # Draw lines on the image
+    x = 0
+    y = img.shape[1]
+
     for line in lines:
         x1, y1, x2, y2 = line[0]
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
-    x1, y1, x2, y2 = lines[len(lines) - 1][0]
-    print('x1:' + str(x1) + ' y1:' + str(y1) + ' x2:' + str(x2) + ' y2:' + str(y2))
-    return img
+        if vertical_or_horizontal(line) == 'horizontal':
+            if min(y1, y2) < y:
+                y = min(y1, y2)
+        elif vertical_or_horizontal(line) == 'vertical':
+            if max(x1, x2) > x:
+                x = max(x1, x2)
+    return img[0:y, x:]
+
+def vertical_or_horizontal(line):
+    x1, y1, x2, y2 = line[0]
+    if abs(x1 - x2) == 0:
+        return 'vertical'
+    m = abs(y1 - y2) / abs(x1 - x2)
+    return 'vertical' if m > 1 else 'horizontal'
 
 def detect_chess(image):
     img = image.copy()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    edged = cv2.Canny(gray, 100, 550)
-    ret, thresh = cv2.threshold(edged, 75, 255, 0)
-    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
     return img
 
+
 def main():
+    # Read and rotate image if you need
     pictures = read_images()
-    show_image(pictures[0], detect_chess(pictures[0]))
+    img_rotate_90_counterclockwise = cv2.rotate(pictures[1], cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+    # Rulers Removed
+    removed_1 = remove_ruler(pictures[0])
+    removed_2 = remove_ruler(img_rotate_90_counterclockwise)
+    removed_3 = remove_ruler(pictures[2])
+
+    # Plotting Q1A
+    # show_image(pictures[0], removed_1)
+    # show_image(img_rotate_90_counterclockwise, removed_2)
+    # show_image(pictures[2], removed_3)
+
+    detected_1 = detect_chess(pictures[0])
+    detected_2 = detect_chess(img_rotate_90_counterclockwise)
+    detected_3 = detect_chess(pictures[2])
+
+    # Plotting Q1B
+    show_image(pictures[0], detected_1)
+    # show_image(img_rotate_90_counterclockwise, detected_2)
+    # show_image(pictures[2], detected_3)
+
 main()
