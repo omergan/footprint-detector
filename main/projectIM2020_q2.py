62a392dea9f6364fb1014cc073affda4b7e872d7
asafjo23 <asafjo23@gmail.com>
2020-02-29 18:32:26 +0200

    q3 starts

--- main/projectIM2020_q2.py
+++ main/projectIM2020_q2.py
@@ -14,24 +14,24 @@ def read_images():
     filenames = [img for img in glob.glob("../images/q2/*.png")]
     images = []
     for filename in filenames:
-        images.append(cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2RGB))
+        images.append(cv2.imread(filename))
     return images
 
 def detect_circles(orig):
     img = orig.copy()
+    temp = orig.copy()
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     bilateral_filtered_image = cv2.bilateralFilter(img, 5, 175, 175)
     circles = cv2.HoughCircles(bilateral_filtered_image, cv2.HOUGH_GRADIENT, 1.2, 75, minRadius=20, maxRadius=60)
-
+    color = (255, 255, 0)
     if circles is not None:
-        print("Circles found")
         circles = np.round(circles[0, :]).astype("int")
 
         for (x, y, r) in circles:
-            cv2.circle(img, (x, y), r, (0, 255, 0), 4)
-            cv2.rectangle(img, (x - 4, y - 4), (x + 4, y + 4), (0, 128, 255), -1)
+            cv2.circle(temp, (x, y), r, color, 10)
+            cv2.rectangle(temp, (x - 4, y - 4), (x + 4, y + 4), (0, 128, 255), -1)
 
-    return img
+    return temp
 
 def show_image(orig, img):
     plt.subplot(231), plt.imshow(orig, cmap='gray'), plt.title("Original")
@@ -46,3 +46,11 @@ images = read_images()
 detected = []
 for image in images:
     detected.append(detect_circles(image))
+
+plt.subplot(231), plt.imshow(images[6], cmap='gray'), plt.title("Original")
+plt.subplot(232), plt.imshow(images[7], cmap='gray'), plt.title("Original")
+plt.subplot(233), plt.imshow(images[8], cmap='gray'), plt.title("Original")
+plt.subplot(234), plt.imshow(detected[6], cmap='gray'), plt.title("Circles")
+plt.subplot(235), plt.imshow(detected[7], cmap='gray'), plt.title("Circles")
+plt.subplot(236), plt.imshow(detected[8], cmap='gray'), plt.title("Circles")
+plt.show()
--- main/projectIM2020_q3.py
+++ main/projectIM2020_q3.py
@@ -1,7 +1,51 @@
 #!/usr/bin/env python
+import numpy as np
+from matplotlib import pyplot as plt
+import glob
+import cv2
 
 """
     @title :        Main for Q3
     @description :  Find a footprint in the database that matches a piece of a footprint
     @output:        Piece of the footprint and the full footprint
 """
+
+def read_images():
+    filenames = [img for img in glob.glob("../images/q3/*.png")]
+    images = []
+    for filename in filenames:
+        images.append(cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2RGB))
+    return images
+
+def show_image(org, img):
+    plt.subplot(121)
+    plt.imshow(org, cmap='gray')
+    plt.subplot(122)
+    plt.imshow(img, cmap='gray')
+    # plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+    plt.show()
+
+def search_engine(database, search):
+    working_copy = search.copy()
+    kernel = np.ones((4, 4), np.uint8)
+    match = []
+    orb = cv2.ORB_create()
+    gray_working_copy = cv2.cvtColor(working_copy, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray_working_copy, 50, 150, apertureSize=3)
+    kp1, des1 = orb.detectAndCompute(edges, None)
+    for candidate in database:
+        closing = cv2.morphologyEx(candidate, cv2.MORPH_CLOSE, kernel)
+        candidate_edges = cv2.Canny(closing, 50, 150, apertureSize=3)
+        kp2, des2 = orb.detectAndCompute(candidate_edges, None)
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        matches = bf.match(des1, des2)
+        matches = sorted(matches, key=lambda x: x.distance)
+        if len(matches) > len(match):
+            match = candidate
+    show_image(match, search)
+
+
+images = read_images()
+db = [images[1], images[3], images[4]]
+search_engine(db, images[0])
+# search_engine(db, images[2])
