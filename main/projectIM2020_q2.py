7a23704ad2d0287ae2ad0fb042efed2b13ac6c54
asafjo23 <asafjo23@gmail.com>
2020-03-02 17:26:21 +0200

    Update projectIM2020_q2.py

--- main/projectIM2020_q2.py
+++ main/projectIM2020_q2.py
@@ -18,39 +18,43 @@ def read_images():
     return images
 
 def detect_circles(orig):
+    # Pre precessing
     img = orig.copy()
     temp = orig.copy()
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     bilateral_filtered_image = cv2.bilateralFilter(img, 5, 175, 175)
-    circles = cv2.HoughCircles(bilateral_filtered_image, cv2.HOUGH_GRADIENT, 1.2, 75, minRadius=20, maxRadius=60)
+    kernel = np.ones((5, 5), np.uint8)
+    erosion = cv2.erode(img, kernel, iterations=1)
+    edges = cv2.Canny(erosion, 50, 150, apertureSize=3)
+
+    circles = cv2.HoughCircles(bilateral_filtered_image, cv2.HOUGH_GRADIENT, 1.2, 1, minRadius=20, maxRadius=60)
     color = (255, 255, 0)
     if circles is not None:
         circles = np.round(circles[0, :]).astype("int")
-
         for (x, y, r) in circles:
-            cv2.circle(temp, (x, y), r, color, 10)
+            cv2.circle(temp, (x, y), r, color, 3)
             cv2.rectangle(temp, (x - 4, y - 4), (x + 4, y + 4), (0, 128, 255), -1)
 
-    return temp
+    else:
+        radii = np.arange(0, 30, 10)
+        for idx in range(len(radii) - 1):
+            minRadius = radii[idx] + 1
+            maxRadius = radii[idx + 1]
+            circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 2.5, 25,
+                                       param1=60, param2=75, minRadius=minRadius, maxRadius=maxRadius)
+            if circles is None:
+                continue
+            circles = np.round(circles[0, :]).astype("int")
+            for (x, y, r) in circles:
+                cv2.circle(temp, (x, y), r, color, 3)
+                cv2.rectangle(temp, (x - 4, y - 4), (x + 4, y + 4), (0, 128, 255), -1)
+    show_image(orig, temp)
 
 def show_image(orig, img):
-    plt.subplot(231), plt.imshow(orig, cmap='gray'), plt.title("Original")
-    plt.subplot(232), plt.imshow(img, cmap='gray'), plt.title("Circles")
-    plt.subplot(233), plt.imshow(orig, cmap='gray'), plt.title("Original")
-    plt.subplot(234), plt.imshow(img, cmap='gray'), plt.title("Circles")
-    plt.subplot(235), plt.imshow(orig, cmap='gray'), plt.title("Original")
-    plt.subplot(236), plt.imshow(img, cmap='gray'), plt.title("Circles")
+    plt.subplot(121), plt.imshow(orig, cmap='gray'), plt.title("Original")
+    plt.subplot(122), plt.imshow(img, cmap='gray'), plt.title("Circles")
     plt.show()
 
 images = read_images()
-detected = []
 for image in images:
-    detected.append(detect_circles(image))
-
-plt.subplot(231), plt.imshow(images[6], cmap='gray'), plt.title("Original")
-plt.subplot(232), plt.imshow(images[7], cmap='gray'), plt.title("Original")
-plt.subplot(233), plt.imshow(images[8], cmap='gray'), plt.title("Original")
-plt.subplot(234), plt.imshow(detected[6], cmap='gray'), plt.title("Circles")
-plt.subplot(235), plt.imshow(detected[7], cmap='gray'), plt.title("Circles")
-plt.subplot(236), plt.imshow(detected[8], cmap='gray'), plt.title("Circles")
-plt.show()
+    detect_circles(image)
