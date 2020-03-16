050e8f672c5fa67c003f4c4b78c5d41b7a9c02b1
RIOT\Omerga <ganon136@gmail.com>
2020-03-16 19:24:17 +0200

    sort of works

--- main/projectIM2020_q4.py
+++ main/projectIM2020_q4.py
@@ -20,19 +20,23 @@ kernel_sharpening = np.array([[-1, -1, -1],
 
 kernel_3x3 = np.ones((3, 3), np.float32) / 9
 
-kernel = np.ones((3, 3), np.uint8)
+kernel = np.ones((2, 2), np.uint8)
 
 images = read_images()
 for img in images:
     temp = img.copy()
+    ret, thresh = cv2.threshold(img, 180, 220, cv2.THRESH_TOZERO)
     gray = cv2.cvtColor(temp, cv2.COLOR_BGR2GRAY)
-    equ = cv2.equalizeHist(gray)
-    sharpened = cv2.filter2D(equ, -1, kernel_sharpening)
-    circles = cv2.HoughCircles(sharpened, cv2.HOUGH_GRADIENT, 1.2, 100, minRadius=20, maxRadius=60)
+    erosion = cv2.erode(gray,kernel,iterations=2)
+    # show_image(img, erosion)
+    # continue
+    # equ = cv2.equalizeHist(gray)
+    # sharpened = cv2.filter2D(equ, -1, kernel_sharpening)
+    circles = cv2.HoughCircles(erosion, cv2.HOUGH_GRADIENT, 1.5, 100, minRadius=20, maxRadius=56)
 
     if circles is not None:
         circles = np.round(circles[0, :]).astype("int")
         for (x, y, r) in circles:
-            draw_circle(x, y, r, sharpened)
-    show_image(equ, sharpened)
+            draw_circle(x, y, r, temp)
+    show_image(img, temp)
     # detect_circles(orig)
